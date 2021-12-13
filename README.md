53f0da93a93648e7339f7d9e5433654be2593a8a
L-alt-del <l.turnell.ritson@gmail.com>
2021-12-13 22:07:50 +0000

    initial

---
+++ README.md
@@ -0,0 +1,77 @@
+<h1 align="center">pomodoro</h1>
+
+<p align="center">
+  bash shell pomodoro prompt
+  </p>
+</div>
+
+## About The Project
+
+Pomodoro is a shell script wich adds a pomodoro style timer to the shell prompt, for example
+```sh
+user@home:~/.pomodoro$ 
+```
+becomes
+```sh
+WORK [||||||----]:user@home:~/.pomodoro$
+```
+then after a specified work period has passed and the  work bar has filled 
+```sh
+PLAY [|---------]:user@home:~/.pomodoro$
+```
+and the cycle repeats. 
+
+### Why?
+
+Advantages,
+
+    1. WORK/PLAY indicator bar is constantly present every time the user hits enter,
+
+    2. scrutinised and simultaneous across all open shells.
+
+## Getting Started
+
+   ```sh
+   mkdir .pomodoro
+   cd .pomodoro
+   git clone https://github.com/LeoTurnell-Ritson/pomodoro.git
+   ```
+   then
+      
+   ```sh
+   souce init.sh
+   ```
+
+   or (recommended) add the following lines to your .bashrc file
+
+   ```sh
+   \#pomodoro
+   if [ -f ~/.pomodoro/init.sh ]; then
+       . ~/.pomodoro/init.sh
+   fi
+   ```
+   
+### Running pomodoro
+
+To begin a new session
+
+```sh
+pomodoro -n
+```
+to join a session from another (separate) shell
+```sh
+pomodoro -j
+```
+to end a session
+
+```sh
+pomodoro -e
+```
+
+note that ending a session in one shell will end the session in all open shells.
+
+For more information just type
+
+```sh
+pomodoro
+```
---
+++ init.sh
@@ -0,0 +1,135 @@
+PROMPT_COMMAND=prompter
+ORG_PS1=$PS1
+let POMO_WORK="25*60"
+let POMO_PLAY="5*60"
+SESSION_FILE="/home/llnt890/.pomodoro/session.sh"
+
+function prompter() {
+    if [ -f "$SESSION_FILE" ] && [ "$POMO" = true ];
+    then
+        export PS1="$(_pomo):$ORG_PS1"
+    else
+        PS1=$ORG_PS1
+    fi
+}
+
+function pomodoro() {
+    if [ "$1" = "-n" ] || [ "$1" = "--new" ]; then
+        export POMO=true
+        POMO_START=$(date +"%s")
+        rm -f "$SESSION_FILE"
+        touch "$SESSION_FILE"
+        echo "SESSION_START=$POMO_START" >> "$SESSION_FILE"
+        echo "CYCLE_START=$POMO_START" >> "$SESSION_FILE"
+        echo "WORK=$POMO_WORK" >> "$SESSION_FILE"
+        echo "PLAY=$POMO_PLAY" >> "$SESSION_FILE"
+    elif [ "$1" = "-j" ] || [ "$1" = "--join" ]; then
+        if [ -f "$SESSION_FILE" ]; then
+            if [ "$POMO" = true ]; then
+                echo "pomodoro: warring: session already joined"
+            else
+                export POMO=true
+            fi
+        else
+            echo "pomodoro: error: no current session file $SESSION_FILE"
+        fi
+    elif [ "$1" = "-e" ] || [ "$1" = "--end" ]; then
+        if [ "$POMO" = true ]; then
+            rm -f "$SESSION_FILE"
+            export POMO=false
+        else
+            echo "pomodoro: error: current session not open"
+        fi
+    elif [ "$1" = "-w" ] || [ "$1" = "--work" ]; then
+        if [ -f "$SESSION_FILE" ]; then
+            . "$SESSION_FILE"
+            rm "$SESSION_FILE"
+            touch "$SESSION_FILE"
+            echo "SESSION_START=$SESSION_START" >> "$SESSION_FILE"
+            echo "CYCLE_START=$CYCLE_START" >> "$SESSION_FILE"
+            echo "WORK=$2" >> "$SESSION_FILE"
+            echo "PLAY=$PLAY" >> "$SESSION_FILE"
+        else
+            echo "pomodoro: error: no current session file $SESSION_FILE"
+        fi
+    elif [ "$1" = "-p" ] || [ "$1" = "--play" ]; then
+        if [ -f "$SESSION_FILE" ]; then
+            . "$SESSION_FILE"
+            rm "$SESSION_FILE"
+            touch "$SESSION_FILE"
+            echo "SESSION_START=$SESSION_START" >> "$SESSION_FILE"
+            echo "CYCLE_START=$CYCLE_START" >> "$SESSION_FILE"
+            echo "WORK=$WORK" >> "$SESSION_FILE"
+            echo "PLAY=$2" >> "$SESSION_FILE"
+        else
+            echo "pomodoro: error: no current session file $SESSION_FILE"
+        fi
+    elif [ "$1" = "-d" ] || [ "$1" = "--display" ]; then
+        if [ -f "$SESSION_FILE" ]; then
+            . "$SESSION_FILE"
+            echo "SESSION_START=$SESSION_START"
+            echo "CYCLE_START=$CYCLE_START"
+            echo "WORK=$WORK"
+            echo "PLAY=$PLAY"
+            POMO_NOW=$(date +"%s")
+            echo "NOW=$POMO_NOW"
+        else
+            echo "pomodoro: error: no current session file $SESSION_FILE"
+        fi
+    else
+        echo "Usage: pomodoro [-n | -j | -e | -d | -w WORK | -p PLAY]"
+        echo ""
+        echo "optional arguments:"
+        echo " -n, --new          start a new session"
+        echo " -j, --join         join are existing session"
+        echo " -e, --end          end the current session"
+        echo " -d, --display      display the current session information"
+        echo " -w, --work WORK    set new work length in seconds"
+        echo " -p, --play PLAY    set new play length in seconds"
+    fi
+}
+
+function _pomo() {
+    . "$SESSION_FILE"
+    POMO_CYCLE=$(expr $WORK + $PLAY)
+    POMO_NOW=$(date +"%s")
+    POMO_CURRENT=$(expr $POMO_NOW - $CYCLE_START)
+    if [ "$POMO_CURRENT" -lt "$WORK" ];
+    then
+        printf "\[\033[01;31m\]WORK ["
+        for i in {1..10};
+        do
+            let BLOCK="(WORK/10)*i";
+            if [ "$BLOCK" -lt "$POMO_CURRENT" ];
+            then
+                printf "|"
+            else
+                printf "-"
+            fi
+        done
+        printf "]\[\033[00m\]"
+    elif [ "$POMO_CURRENT" -lt "$POMO_CYCLE" ];
+    then
+        printf "\[\033[01;36m\]PLAY [";
+        for i in {1..10};
+        do
+            let BLOCK="((PLAY/10)*i)+WORK";
+            if [ "$BLOCK" -lt "$POMO_CURRENT" ];
+            then
+                printf "|"
+            else
+                printf "-"
+            fi
+        done
+        printf "]\[\033[00m\]"
+    else
+        CYCLE_START=$(expr $CYCLE_START + $POMO_CYCLE)
+        rm "$SESSION_FILE"
+        touch "$SESSION_FILE"
+        echo "SESSION_START=$SESSION_START" >> "$SESSION_FILE"
+        echo "CYCLE_START=$CYCLE_START" >> "$SESSION_FILE"
+        echo "WORK=$WORK" >> "$SESSION_FILE"
+        echo "PLAY=$PLAY" >> "$SESSION_FILE"
+        printf "\[\033[01;36m\]PLAY [||||||||||]\[\033[00m\]";
+    fi
+}
